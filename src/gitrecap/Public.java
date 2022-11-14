e92e959609cbe95d378c742283738d873b39f08c
Ataullah Atal <atalirfan@gmail.com>
2022-11-14 14:37:31 -0500

    hello world

--- src/gitrecap/Public.java
+++ src/gitrecap/Public.java
@@ -3,6 +3,7 @@ package gitrecap;
 public class Public {
 	public static void main(String[] args) {
 		System.out.println("this is new branch");
+		System.out.println("this is inside branch");
 	}
 
 }
